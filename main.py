070a186ab66d8a1c2c4438504f531f2adf16d6cd
yanghao <yanghao@paxsz.com>
2022-08-22 16:36:56 +0800

    code refactor

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-menstrual_period = os.environ['MENSTRUAL_PERIOD']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
